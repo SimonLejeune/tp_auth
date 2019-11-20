1c060ff876b0aed1940e6e9a9bae625cb92e5ba6
Simon Lejeune <simon.lejeune@epitech.eu>
2019-11-19 19:30:47 -0500

    first push

---
+++ TP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
